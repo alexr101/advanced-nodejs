3986fabe42dd1aabfbd74afdf4d79d2e551ab61e
alexr101 <alexonezero@outlook.com>
2017-09-23 21:27:45 -0400

    requiring json and c++ files

---
+++ Require/JSON-and-C++/app.js
@@ -0,0 +1,9 @@
+// var req = require('file');
+
+// Nodejs will look for file types in this order
+// file.js
+// file.json 
+// file.node
+
+var json = require('data.json');
+console.log(json.data1);
