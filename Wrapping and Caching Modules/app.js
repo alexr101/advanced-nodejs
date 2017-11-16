91dae7f1f467df93eb90319d37070f89e25583f0
alexr101 <alexonezero@outlook.com>
2017-11-16 10:43:20 -0500

    added 2 separate ways to override cache of a module

--- Wrapping and Caching Modules/app.js
+++ Wrapping and Caching Modules/app.js
@@ -1,3 +1,20 @@
 var stars = require("./printStars");
 
 stars(15, "STARRS");
+
+
+// Node js caches modules so requiring it twice won't run the code again.
+// You can delete cache with the methods below
+var chachedAstroBoy = require("./requireCache");
+// console.log(require.cache);
+
+// Solution1 :not most efficient
+// delete require.cache["C:\\Users\\alexp\\Documents\\more-about-cloned\\node\\advanced-nodejs\\Wrapping and Caching Modules\\requireCache.js"];
+var chachedAstroBoy = require("./requireCache");
+
+// SOLUTION 2
+// Wrap the log under module.exports, and call it from here
+
+require('./cachedAstroBoy')();
+require('./cachedAstroBoy')();
+
--- Wrapping and Caching Modules/printStars.js
+++ Wrapping and Caching Modules/printStars.js
@@ -7,13 +7,13 @@ const printStars = (amount, header) => {
   }
   // var stars = "*".repeat(stars)
 
-  console.log(stars);
-  console.log(header);
+  console.log(stars)
+  console.log(header)
   console.log(stars)
 }
 
-console.log("require.main")
-console.log(require.main)
+// console.log("require.main")
+// console.log(require.main)
 
 if(require.main == module) {
   printStars(process.argv[2], process.argv[3]);
