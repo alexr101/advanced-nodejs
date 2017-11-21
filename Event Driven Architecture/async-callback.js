53f2745d83a96531a249a5ba5f91b42034e64956
alexr101 <alexonezero@outlook.com>
2017-11-21 16:36:24 -0500

    add odd numbers count fn before converting to promise based fn

---
+++ Event Driven Architecture/async-callback.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const readFileAsArray = function(file, cb) {
+  fs.readFile(file, function(err, data) {
+    if(err) return cb(err);
+
+    const lines = data.toString().trim().split('\n');
+    cb(null, lines);
+  })
+}
+
+readFileAsArray('./numbers.txt', (err, lines) => {
+  if(err) throw err;
+
+  const numbers = lines.map(Number);
+  const oddNumbers = numbers.filter(number => number%2 === 1);
+  console.log('odd numbers cound: ', oddNumbers.length);
+})
